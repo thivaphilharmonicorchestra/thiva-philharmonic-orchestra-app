53761c7540b05cac75b708f8232be72e284a78b9
Thiva Philharmonic Orchestra <thivaphilharmonic@gmail.com>
2026-09-18 17:37:44 +0300

    v35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'thiva-philharmonic-v33';
+const CACHE_VERSION = 'thiva-philharmonic-v35';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PDF_CACHE = `${CACHE_VERSION}-pdfs`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
